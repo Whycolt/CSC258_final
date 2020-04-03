e07eaaa92321e34bbf3b1f4168dc1ef356417552
macolt <colt.ma@mail.utoronto.ca>
2020-04-02 22:33:47 -0400

    Add other part of code

--- flappybird.s
+++ flappybird.s
@@ -5,7 +5,7 @@
 #
 # Group members:
 # - Student 1: Colt Ma, 1004180520
-# - Student 2 (if any): Name, Student Number
+# - Student 2 (if any): Alexandru Marcu, 1004442999
 #
 # Bitmap Display Configuration:
 # - Unit width in pixels: 8					     
@@ -35,17 +35,21 @@
 # - bid 4 units long, 3 units high
 
 # - pipe 1 memory -> $s0
+# - bird memory -> $s1
 .data
 	birdColor: .word 0xD1C429	# Color of bird
 	pipeColor: .word 0x74C029	# Color of pipes
 	skyColor: .word 0x72C7D0	# Color of sky
 	s0: .space 8			# Space allocated for Pipe 1
+	s1: .space 8			# Space allocated for Bird
+
 .globl main
 .text
 
 main:			# Load memory addresses
 	la $s0, s0			# Store pipe 1 memory address at $s0
-
+	la $s1, s1
+	
 full_fill_prep:		# Prep for filling background
 	add $t0, $zero, $gp		# Load bitmap address to $t0
 	lw $t1, skyColor		# Load sky color to #t1
@@ -56,7 +60,12 @@ full_fill:		# Loop for filling background
 	sw $t1, 0($t0)			# paint a unit on bitmap
 	addi $t0, $t0, 4		# increment bitmap address
 	addi $t2, $t2, 1		# increment counter
-	bne $t2, $t3, full_fill 	# loop drawing background		
+	bne $t2, $t3, full_fill 	# loop drawing background
+	
+bird_start_prep: 		# Loop for drawing bird at its starting position
+	add $t0, $zero, $zero
+	addi $t0, $t0, 1948		# The bird will always first load in here
+	sw $t0, 0($s1)			# Save into memory		
 
 draw_pipe1_prep:	# Initiating a pipe	
 	li $a1, 16 			# Load random number max to $a1
@@ -71,6 +80,9 @@ game_loop:		# --GAME LOOP--
 	li $v0, 32			# Load pause syscall to $v0
 	li $a0, 160			# Load pause duration to $a0
 	syscall
+	#-- CHECK INPUT FIRST, SET 8($s1) (save old pos in 12($s1), and check >< to know if to color top or bottom)
+	jal input_check
+	jal draw_bird
 	jal pipe1_branch		# Draw Pipe 1 method
 	j game_loop			# loop game
 
@@ -125,6 +137,74 @@ pipe1_store:			# Store pipe 1 variables into memory
 	sw $t3 0($s0)			# Save pipe gap top into memory
 	sw $t4 4($s0)			# Save pipe location into memory
 	jr $ra				# Return to game loop
+	
+	
+input_check:
+	lw $t0, 0xffff0000
+	beq $t0, $zero, bird_fall
+	
+	# Check if they pressed "f"
+	lw $t0, 0xffff0004
+	add $t1, $zero, $zero
+	addi $t1, $t1, 102
+	bne $t0, $t1, bird_fall
+
+	# If here, than there was input to the keyboard, and we must "raise" the bird
+	add $t0, $zero, $zero
+
+	lw $t0, 0($s1)
+	addi $t0, $t0, 128
+	sw $t0, 4($s1)
+	subi $t0, $t0, 128
+	subi $t0, $t0, 128
+	sw $t0, 0($s1)
+	
+	j DONE
+	
+bird_fall:
+	add $t0, $zero, $zero
+
+	lw $t0, 0($s1)
+	sw $t0, 4($s1)
+	addi $t0, $t0, 128
+	sw $t0, 0($s1)
+DONE:
+	sw $zero, 0xffff0000 		# Resetting keyboard input
+	jr $ra
+	
+draw_bird:
+	add $t0, $zero, $gp
+	
+	add $t1, $zero, $zero 		# Current position of bird
+	lw $t1, 0($s1) 
+	
+	add $t2, $zero, $zero		# Old position (to re-color back the sky)
+	lw $t2, 4($s1)
+	
+	# Color the previous (old first row back to sky)
+	lw $t3, skyColor
+	add $t4, $t0, $t2
+	sw $t3, 0($t4)
+	sw $t3, 4($t4)
+	sw $t3, 8($t4)
+	sw $t3, 12($t4)
+	
+	lw $t3, birdColor
+	add $t4, $t0, $t1
+	
+	# First row
+	sw $t3, 0($t4)
+	sw $t3, 4($t4)
+	sw $t3, 8($t4)
+	sw $t3, 12($t4)
+	
+	# Second row
+	sw $t3, 128($t4)
+	sw $t3, 132($t4)
+	sw $t3, 136($t4)
+	sw $t3, 140($t4)
+	
+	jr $ra
 
 Exit:
 	li $v0, 10 # terminate the program gracefully
